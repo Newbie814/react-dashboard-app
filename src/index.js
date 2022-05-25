11f6fea80a2ca88aef896c1f48865b0159a8a296
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-05-25 19:02:21 -0400

    set up context provider and sidebar close function

--- src/index.js
+++ src/index.js
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 import App from './App';
+import { ContextProvider } from './contexts/ContextProvider';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <ContextProvider>
+    <App />
+  </ContextProvider>,
+  document.getElementById('root')
+);
 
 // import React from 'react';
 // import ReactDOM from 'react-dom/client';
